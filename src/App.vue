e054d92143dafc01dc945f45f234d4864adf4862
sglazewski <szprx1910@gmail.com>
2023-06-16 01:03:27 +0200

    display

--- src/App.vue
+++ src/App.vue
@@ -11,22 +11,21 @@ import Navigator from "./components/Navigator.vue";
 
 <style lang="scss">
 * {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  font-family: "Raleway", sans-serif;
-  font-weight: 400;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "Raleway", sans-serif;
+    font-weight: 400;
 }
 
 .app {
-  min-height: 100vh;
-  position: relative;
-  background-color: #2c3e50;
+    min-height: 100vh;
+    position: relative;
+    background-color: #f1f1f1;
 }
-
 .container {
-  padding: 0 20px;
-  max-width: 1140px;
-  margin: 0 auto;
+    padding: 0 20px;
+    max-width: 1140px;
+    margin: 0 auto;
 }
 </style>
--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,27 +1,45 @@
 <template>
-    <header :class="{ 'scrolled-nav' : scrollPosition}"></header>
-    <nav>
-        <div class="branding">
-            <img src="../assets/logo.png" alt="">
-        </div>
-        <ul v-show="!mobile" class="navigation">
-            <li><router-link class="link" :to="{name: 'Home'}">Home</router-link></li>
-            <li><router-link class="link" :to="{name: 'ShopList'}">About</router-link></li>
-            <li><router-link class="link" :to="{name: 'ToBuy'}">Portfolio</router-link></li>
-            <li><router-link class="link" :to="{name: 'Bought'}">Contacts</router-link></li>
-        </ul>
-        <div class="icon">
-            <i @click="toggleMobileNav" v-show="mobile" class="far fa-bars" :class="{'icon-active' : mobileNav}"></i>
-        </div>
-        <transition name="mobile-nav">
-            <ul v-show="mobileNav" class="dropdown-nav">
-                <li><router-link class="link" :to="{name: 'Home'}">Home</router-link></li>
-                <li><router-link class="link" :to="{name: 'ShopList'}">About</router-link></li>
-                <li><router-link class="link" :to="{name: 'ToBuy'}">Portfolio</router-link></li>
-                <li><router-link class="link" :to="{name: 'Bought'}">Contacts</router-link></li>
+    <header :class="{ 'scrolled-nav' : scrollPosition}">
+        <nav>
+            <div class="branding">
+                <img src="../assets/logo.png" alt="">
+            </div>
+            <ul v-show="!mobile" class="navigation">
+                <li>
+                    <router-link class="link" :to="{name: 'Home'}">Home</router-link>
+                </li>
+                <li>
+                    <router-link class="link" :to="{name: 'ShopList'}">About</router-link>
+                </li>
+                <li>
+                    <router-link class="link" :to="{name: 'ToBuy'}">Portfolio</router-link>
+                </li>
+                <li>
+                    <router-link class="link" :to="{name: 'Bought'}">Contacts</router-link>
+                </li>
             </ul>
-        </transition>
-    </nav>
+            <div class="icon">
+                <i @click="toggleMobileNav" v-show="mobile" class="far fa-bars"
+                   :class="{'icon-active' : mobileNav}"></i>
+            </div>
+            <transition name="mobile-nav">
+                <ul v-show="mobileNav" class="dropdown-nav">
+                    <li>
+                        <router-link class="link" :to="{name: 'Home'}">Home</router-link>
+                    </li>
+                    <li>
+                        <router-link class="link" :to="{name: 'ShopList'}">About</router-link>
+                    </li>
+                    <li>
+                        <router-link class="link" :to="{name: 'ToBuy'}">Portfolio</router-link>
+                    </li>
+                    <li>
+                        <router-link class="link" :to="{name: 'Bought'}">Contacts</router-link>
+                    </li>
+                </ul>
+            </transition>
+        </nav>
+    </header>
 </template>
 
 <script>
@@ -34,27 +52,32 @@ export default {
             return Home
         }
     },
-    data(){
-        return{
+    data() {
+        return {
             scrollPosition: null,
             mobile: true,
             mobileNav: null,
             windowWidth: null,
         }
-    }
+    }, methods: {
+        toggleMobileNav() {
+            this.mobileNav = !this.mobileNav;
+        }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-header{
-    background-color:  rgba(0,0,0,0.8);
-    z-index: 99;
-    width: 100%;
-    position: fixed;
-    transition: 0.5s ease all;
-    color: #ffffff;
-}
-nav{
+header {
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 99;
+  width: 100%;
+  position: fixed;
+  transition: 0.5s ease all;
+  color: #ffffff;
+
+  nav {
+    position: relative;
     display: flex;
     flex-direction: row;
     padding: 12px 0;
@@ -62,37 +85,92 @@ nav{
     width: 90%;
     margin: 0 auto;
     @media(min-width: 1140px) {
-        max-width: 1140px;
+      max-width: 1140px;
     }
+
     ul,
-    .link{
-    font-weight: 500;
-        color: #ffffff;
-        list-style: none;
-        text-decoration: none;
+    .link {
+      font-weight: 500;
+      color: #ffffff;
+      list-style: none;
+      text-decoration: none;
     }
-    li{
-        text-transform: uppercase;
-        padding: 16px;
-        margin-left: 16px;
+
+    li {
+      text-transform: uppercase;
+      padding: 16px;
+      margin-left: 16px;
     }
-    link{
-        font-size: 14px;
-        transition: 0.5s ease all;
-        padding-bottom: 4px;
-        border-bottom: 1px solid transparent;
-        &:hover{
-            color: #00afea;
-            border-color: #00afea;
-        }
+
+    link {
+      font-size: 14px;
+      transition: 0.5s ease all;
+      padding-bottom: 4px;
+      border-bottom: 1px solid transparent;
+
+      &:hover {
+        color: #00afea;
+        border-color: #00afea;
+      }
     }
-    .branding{
+
+    .branding {
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 50px;
+        transition: .5s ease all;
+      }
+    }
+
+    .navigation {
+      display: flex;
+      align-items: center;
+      flex: 1;
+      justify-content: flex-end;
+    }
+
+    .icon {
+      display: flex;
+      align-items: center;
+      position: absolute;
+      top: 0;
+      right: 24px;
+      height: 100%;
+      color: #ffffff;
+
+      i {
+        cursor: pointer;
+        font-size: 24px;
+        transition: 0.8s ease all;
+      }
+
+      .icon-active {
+        transform: rotate(180deg);
+      }
+
+      .dropdown-nav {
         display: flex;
-        align-items: center;
-        img{
-            width: 50px;
-            transition: .5s ease all;
+        flex-direction: column;
+        position: fixed;
+        width: 100%;
+        max-width: 250px;
+        height: 100%;
+        background-color: #ffffff;
+        top: 0;
+        left: 0;
+
+        li {
+          margin-left: 0;
+
+          .link {
+            color: #000;
+          }
         }
+      }
     }
+  }
 }
+
 </style>
