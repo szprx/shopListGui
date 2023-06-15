7cb4e9c6abca9d950fabdded66b410577eb7351e
sglazewski <szprx1910@gmail.com>
2023-06-16 01:04:48 +0200

    displays

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -67,110 +67,134 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-header {
-  background-color: rgba(0, 0, 0, 0.8);
-  z-index: 99;
-  width: 100%;
-  position: fixed;
-  transition: 0.5s ease all;
-  color: #ffffff;
-
-  nav {
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    padding: 12px 0;
+<style lang="scss" scoped>header {
+    background-color: rgba(0, 0, 0, 0.8);
+    z-index: 99;
+    width: 100%;
+    position: fixed;
     transition: 0.5s ease all;
-    width: 90%;
-    margin: 0 auto;
-    @media(min-width: 1140px) {
-      max-width: 1140px;
-    }
+    color: #fff;
 
-    ul,
-    .link {
-      font-weight: 500;
-      color: #ffffff;
-      list-style: none;
-      text-decoration: none;
-    }
-
-    li {
-      text-transform: uppercase;
-      padding: 16px;
-      margin-left: 16px;
-    }
-
-    link {
-      font-size: 14px;
-      transition: 0.5s ease all;
-      padding-bottom: 4px;
-      border-bottom: 1px solid transparent;
-
-      &:hover {
-        color: #00afea;
-        border-color: #00afea;
-      }
-    }
-
-    .branding {
-      display: flex;
-      align-items: center;
-
-      img {
-        width: 50px;
-        transition: .5s ease all;
-      }
-    }
-
-    .navigation {
-      display: flex;
-      align-items: center;
-      flex: 1;
-      justify-content: flex-end;
-    }
-
-    .icon {
-      display: flex;
-      align-items: center;
-      position: absolute;
-      top: 0;
-      right: 24px;
-      height: 100%;
-      color: #ffffff;
-
-      i {
-        cursor: pointer;
-        font-size: 24px;
-        transition: 0.8s ease all;
-      }
-
-      .icon-active {
-        transform: rotate(180deg);
-      }
-
-      .dropdown-nav {
+    nav {
+        position: relative;
         display: flex;
-        flex-direction: column;
-        position: fixed;
-        width: 100%;
-        max-width: 250px;
-        height: 100%;
-        background-color: #ffffff;
-        top: 0;
-        left: 0;
+        flex-direction: row;
+        padding: 12px 0;
+        transition: 0.5s ease all;
+        width: 90%;
+        margin: 0 auto;
+        @media (min-width: 1140px) {
+            max-width: 1140px;
+        }
+
+        ul,
+        .link {
+            font-weight: 500;
+            color: #fff;
+            list-style: none;
+            text-decoration: none;
+        }
 
         li {
-          margin-left: 0;
-
-          .link {
-            color: #000;
-          }
+            text-transform: uppercase;
+            padding: 16px;
+            margin-left: 16px;
         }
-      }
+
+        .link {
+            font-size: 14px;
+            transition: 0.5s ease all;
+            padding: 4px;
+            border-bottom: 1px solid transparent;
+
+            &:hover {
+                color: #00afea;
+                border-color: #00afea;
+            }
+        }
+        .branding {
+            display: flex;
+            align-items: center;
+
+            img {
+                width: 50px;
+                transition: 0.5s ease all;
+            }
+        }
+
+        .navigation {
+            display: flex;
+            align-items: center;
+            flex: 1;
+            justify-content: flex-end;
+        }
+        .icon {
+            display: flex;
+            align-items: center;
+            position: absolute;
+            top: 0;
+            right: 24px;
+            height: 100%;
+
+            i {
+                cursor: pointer;
+                font-size: 24px;
+                transition: 0.5 ease all;
+            }
+        }
+
+        .icon-active {
+            transform: rotate(180deg);
+        }
+
+        .dropdown-nav {
+            display: flex;
+            flex-direction: column;
+            position: fixed;
+            width: 100%;
+            max-width: 250px;
+            height: 100%;
+            background-color: #fff;
+            top: 0;
+            left: 0;
+
+            li {
+                margin-left: 0;
+                .link {
+                    color: #000;
+                }
+            }
+        }
+        .mobile-nav-enter-active,
+        .mobile-nav-leave-active {
+            transition: 1s ease all;
+        }
+
+        .mobile-nav-enter-from,
+        .mobile-nav-leave-to {
+            transform: translateX(-250px) ;
+        }
+
+        .mobile-nav-enter-to {
+            transform: translateX(0) ;
+        }
     }
-  }
 }
 
-</style>
+.scrolled-nav {
+    background-color: #000;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+    nav {
+        padding: 8px 0;
+
+        .branding {
+            img {
+                width: 40px;
+                box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+                0 2px 4px -1px rgba(0, 0, 0, 0.06);
+            }
+        }
+    }
+}</style>
