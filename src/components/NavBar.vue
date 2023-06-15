a88f150cc6ba9232b6a4dbaa0d7f2c2f69626d20
sglazewski <szprx1910@gmail.com>
2023-06-15 03:37:07 +0200

    navbar v3

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -76,7 +76,7 @@ nav .icon {
 
 /* The "responsive" class is added to the topnav with JavaScript when the user clicks on the icon. This class makes the topnav look good on small screens (display the links vertically instead of horizontally) */
 @media screen and (max-width: 600px) {
-    .nav.responsive {position: relative;}
+    nav.responsive {position: relative;}
     .nav.responsive a.icon {
         position: absolute;
         right: 0;
