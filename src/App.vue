7f3aa5bd878bb73bfcfa486b5275b82af4e10115
sglazewski <szprx1910@gmail.com>
2023-06-17 20:58:05 +0200

    responsive

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,16 @@ import Navigator from "./components/Navigator.vue";
     </div>
 </template>
 
-<style lang="scss">
-* {
+<style lang="scss">* {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
     font-family: "Raleway", sans-serif;
     font-weight: 400;
 }
-.site{
-    height: 100vh;
+
+.site {
+    min-height: 100vh;
 }
 
 .app {
@@ -26,60 +26,84 @@ import Navigator from "./components/Navigator.vue";
     position: relative;
     background-color: #f1f1f1;
 }
+
 .container {
     padding: 0 20px;
     max-width: 1140px;
     margin: 0 auto;
-    height: 100vh;
+    min-height: 100vh;
 }
 
 .hero {
     background-image: url("./assets/hero-bg.png");
     background-attachment: fixed;
     position: relative;
-    height: 100vh;
+}
 
-    img {
-        object-fit: cover;
-        height: 100%;
-        width: 100%;
+img {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+}
+
+.hero-text {
+    display: flex;
+    flex-direction: column;
+    color: #fff;
+    padding-top: 80px;
+}
+
+@media (min-width: 980px) {
+    .hero-text {
+        padding-top: 100px;
+    }
+}
+
+h4 {
+    text-transform: uppercase;
+    font-size: 22px;
+    padding-bottom: 4px;
+}
+
+h2 {
+    font-size: 50px;
+}
+
+@media (min-width: 550px) {
+    h2 {
+        font-size: 80px;
+    }
+}
+
+hr:nth-child(2) {
+    max-width: 365px;
+    margin-bottom: 16px;
+}
+
+hr:nth-child(4) {
+    height: 6px;
+    background-color: #fff;
+    border: none;
+    max-width: 85px;
+    margin-top: 16px;
+}
+
+@media (max-width: 768px) {
+    .container {
+        padding: 0 10px;
+        max-width: 100%;
     }
 
     .hero-text {
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        color: #fff;
-        padding-top: 80px;
-        @media (min-width: 980px) {
-            padding-top: 100px;
-        }
+        padding-top: 40px;
+    }
 
-        h4 {
-            text-transform: uppercase;
-            font-size: 22px;
-            padding-bottom: 4px;
-        }
+    h4 {
+        font-size: 18px;
+    }
 
-        h2 {
-            font-size: 50px;
-            @media (min-width: 550px) {
-                font-size: 80px;
-            }
-        }
-
-        hr:nth-child(2) {
-            max-width: 365px;
-            margin-bottom: 16px;
-        }
-
-        hr:nth-child(4) {
-            height: 6px;
-            background-color: #fff;
-            border: none;
-            max-width: 85px;
-            margin-top: 16px;
-        }
+    h2 {
+        font-size: 40px;
     }
 }
 </style>
