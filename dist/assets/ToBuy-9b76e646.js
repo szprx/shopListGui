5eae6894a0ab7fe05d96973aab8d6b7aefccf86f
sglazewski <szprx1910@gmail.com>
2023-06-17 12:43:27 +0200

    v28

--- dist/assets/ToBuy-9b76e646.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as t,c as n,a as e,d as o}from"./index-0c3692b8.js";const a={name:"ToBuy"},c=e("section",{class:"hero"},[e("div",{class:"hero-text container"},[e("h4",null,"Vue Responsive Nav Menu"),e("h2",null,[o(" With Vue "),e("br"),o(" Animations ")])])],-1),r=[c];function i(_,l,u,d,p,h){return t(),n("div",null,r)}const m=s(a,[["render",i]]);export{m as default};
