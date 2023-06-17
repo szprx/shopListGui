dcf0650f9e9e1217be3c464add92a7a50b209574
sglazewski <szprx1910@gmail.com>
2023-06-17 12:43:27 +0200

    v28

---
+++ assets/Bought-b9b04313.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,c as n,a as e,d as t}from"./index-1205ed79.js";const a={name:"Bought"},c=e("section",{class:"hero"},[e("div",{class:"hero-text container"},[e("h4",null,"Vue Responsive Nav Menu"),e("h2",null,[t(" With Vue "),e("br"),t(" Animations ")])])],-1),r=[c];function i(_,l,u,d,h,p){return s(),n("div",null,r)}const m=o(a,[["render",i]]);export{m as default};
